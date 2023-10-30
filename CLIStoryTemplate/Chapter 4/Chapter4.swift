8d1063092c55a1f2e40fab31c604dbaded10c271
Kahlil Floyd <khfloyd05@gmail.com>
2023-10-30 11:21:28 -0400

    update

--- CLIStoryTemplate/Chapter 4/Chapter4.swift
+++ CLIStoryTemplate/Chapter 4/Chapter4.swift
@@ -5,7 +5,44 @@
 
 import Foundation
 
+//astronaut names
+var astronautCommander = "Reid Wiseman"
+var astronautPilot = "Victor Glover"
+var astronautMissionSpecialistOne = "Christina Koch"
+var astronautMissionSpecialistTwo = "Jeremy Hansen"
+
+//mission objectives
+var takingOff = true
+var engineFailue = true
+var cureAdministered = true
+var dosageOneWrong = true
+var dosageTwoCorrect = true
+
 func chapterFour() {
-print("Hello")
+print("This is Kahlil's branch")
     print("This is Kahlil's branch")
 }
+
+let ourName = "Spaceranger"
+
+
+var cure = "Vitamin M"
+
+
+
+if cure == "Administered"
+    print("The Planet is Saved!")
+{
+    
+    print ("objective failed")
+    
+    var missionPieces: [String] = ["cure", "fuel","Dosage" ]
+    var cureDose: <#type#> = [1, 5, 10]
+    
+    
+    print("The space ranger is traveling back to earth from Mars with the \(cure)")
+    print("The space ranger must provide the proper \(missionPieces[cureDose - 2])")
+    
+    print "After following explicit instructions given by astrobiologist Sarah Larson, Reid, Victor, Christina, and Jeremy set their sights back to earth with the cure that could save humanity in hand."
+    print "On the trip back to earth however, Larson relayed a message to the team that they must administer the cure due to a lack of time, but if they use the wrong amount, it will have the opposite effect and make the crisis worse to alleviate."
+    print "Upon hearing this information, the spaceship's engine starts to notify the crew of failing status"
