ac45e47ccd8490af4b6cc9c4f1d71dd9ca06876e
Abdulaziz Jamaleddin <abduljeddin@gmail.com>
2023-10-31 10:04:24 -0400

    Changes

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -22,15 +22,32 @@ func chapterOne() {
     }
     var infectionPlanets = ["Smar","Oamena","Verzyai"]
     let infectionPlanet = infectionPlanets[Int.random(in: 0...2)]
+sleep(2)
+print("As the climate crisis intensified, the Earth's ice caps were melting at an alarming rate.")
+    sleep(2)
+    print("It wasn't just the rising sea levels and extreme weather events that worried scientists now. ")
+    sleep(2)
+
+    print("It was the revelation that one of the ancient ice caps contained a virus, a virus unlike anything ever seen before. Its origin? An extraterrestrial planet called \(infectionPlanet).")
     
-print("As the climate crisis intensified, the Earth's ice caps were melting at an alarming rate. It wasn't just the rising sea levels and extreme weather events that worried scientists now. It was the revelation that one of the ancient ice caps contained a virus, a virus unlike anything ever seen before. Its origin? An extraterrestrial planet called \(infectionPlanet).")
-//    
-    print("""
-Dr. Sarah Larson, a brilliant astrobiologist, had been studying the virus for months. It had already infected a significant portion of the population, causing strange symptoms and leaving those afflicted in a state of paralysis, unable to communicate. Some called it the "Smar Virus," and its origins were shrouded in mystery.
+//
+    print("Dr. Sarah Larson, a brilliant astrobiologist, had been studying the virus for months.")
+    sleep(2)
 
-Sarah worked tirelessly, analyzing the virus's genetic code, and she discovered something shocking. The virus was not naturally occurring on Earth. Its complex structure and genetic makeup suggested an extraterrestrial origin. She couldn't believe it; she had discovered evidence of alien life.
+    print("It had already infected a significant portion of the population, causing strange symptoms and leaving those afflicted in a state of paralysis, unable to communicate. ")
+    sleep(2)
 
-After sharing her findings with a select group of scientists, they set their sights on a new mission: to trace the virus back to its source and find a cure. It became clear that the antidote for the virus might also be extraterrestrial in origin. The fate of humanity rested on their shoulders.
-""")
+    print("Some called it the \(infectionPlanet) Virus, and its origins were shrouded in mystery.")
+    sleep(2)
+
+    
+
+print("Sarah worked tirelessly, analyzing the virus's genetic code, and she discovered something shocking. The virus was not naturally occurring on Earth. Its complex structure and genetic makeup suggested an extraterrestrial origin. She couldn't believe it; she had discovered evidence of alien life.")
+    sleep(2)
+
+
+print("After sharing her findings with a select group of scientists, they set their sights on a new mission: to trace the virus back to its source and find a cure. It became clear that the antidote for the virus might also be extraterrestrial in origin. The fate of humanity rested on their shoulders.")
+    sleep(2)
+
   
 }
